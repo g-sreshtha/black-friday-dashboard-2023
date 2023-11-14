2937d2e09a75c1ba99cf302b008d7fc935c19a7a
okersh3 <oliver.kershaw83@thehutgroup.com>
2023-11-14 16:23:31 +0000

    Added functionality for hovering over countries - need to add Tippy

--- src/MapChart.jsx
+++ src/MapChart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ComposableMap,
   Geographies,
@@ -7,36 +7,85 @@ import {
   Graticule,
 } from 'react-simple-maps';
 
+import Tippy from '@tippyjs/react';
+
 const countries = {
-  name: 'United Kingdom', // ISO code for United Kingdom
-  color: '#000000', // Black color
+  name: 'United Kingdom',
+  color: '#000000',
 };
 
 const geoUrl = 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json';
 
 const MapChart = () => {
-  return (
-    <div style={{ backgroundColor: 'transparent' }}>
-      <ComposableMap data-tip="">
-        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => {
-              const isEngland = geo.properties.name === countries.name;
+  const [product, setProduct] = useState(undefined);
+  console.log(product);
 
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={isEngland ? countries.color : '#F5F4F6'}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ComposableMap>
-    </div>
+  useEffect(() => {
+    console.log({ product });
+
+    const el = document.getElementById(product);
+    console.log(el);
+
+    Tippy(`#${product}`, {
+      content: { product },
+    });
+  }, [product]);
+
+  // const handleMouseEnter = () => {};
+  return (
+    <>
+      <button id="test">test me</button>
+      <div
+        style={{
+          backgroundColor: 'transparent',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ComposableMap
+          data-tip=""
+          style={{
+            height: '1000px',
+          }}
+        >
+          <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+          <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => {
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    id={geo.rsmKey}
+                    fill={
+                      geo.properties.name === countries.name
+                        ? countries.color
+                        : '#F5F4F6'
+                    }
+                    onMouseEnter={() => {
+                      const product = geo.properties.name;
+                      setProduct(geo.rsmKey);
+                    }}
+                    onMouseLeave={() => {
+                      setProduct(undefined);
+                    }}
+                    style={{
+                      hover: {
+                        fill: '#000000',
+                        outline: 'none',
+                      },
+                    }}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
+    </>
   );
 };
 
