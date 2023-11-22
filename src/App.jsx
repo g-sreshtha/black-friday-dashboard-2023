9ad58ac1b42933658bc2deea1033ec7e3df1d1e3
ugne-simanaviciute <ugne.simanaviciute54@thehutgroup.com>
2023-11-22 23:08:20 +0000

    Testing video fix

--- src/App.jsx
+++ src/App.jsx
@@ -368,6 +368,10 @@ export const App = () => {
             <h3>Current Top 10 Brands</h3>
             <ol>{top10brands}</ol>
           </Tooltip>
+          <video autoPlay muted loop id="video-background">
+            <source src="pexels_videos_2611250 (2160p).mp4" type="video/mp4" />
+            Your Browser Type is not supported
+          </video>
         </div>
       </div>
     </>
