cd13cd2ca73b633c9658cf0277da8f495916f283
okersh3 <oliver.kershaw83@thehutgroup.com>
2023-11-22 23:16:35 +0000

    Removed loop as testing

--- src/App.jsx
+++ src/App.jsx
@@ -298,7 +298,7 @@ export const App = () => {
 
   return (
     <>
-      <video autoPlay muted loop id="video-background">
+      <video autoPlay muted id="video-background">
         <source src="pexels_videos_2611250 (2160p).mp4" type="video/mp4" />
       </video>
       <div style={{ display: 'grid' }}>
